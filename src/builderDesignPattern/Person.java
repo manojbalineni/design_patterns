060ae6a56baca97e05582c509008a299ab9d4aa7
Manoj <manoj.balineni04@gmail.com>
2025-04-29 17:17:40 +0530

    Initial commit

---
+++ src/builderDesignPattern/Person.java
@@ -0,0 +1,47 @@
+package builderDesignPattern;
+
+public class Person {
+
+    private String name;
+    private String email;
+    private int age;
+
+
+    public Person(Builder builder){
+        this.name = builder.name;
+        this.age = builder.age;
+
+    }
+
+
+    public static class Builder{
+
+        private String name;
+        private String email;
+        private int age;
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder setEmail(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Builder setAge(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " years old)";
+    }
+}
---
+++ src/builderDesignPattern/PersonMain.java
@@ -0,0 +1,13 @@
+package builderDesignPattern;
+
+public class PersonMain {
+
+    public static void main(String[] args) {
+        Person person = new Person.Builder()
+                .setAge(23)
+                .setEmail("manoj@gmail.com")
+                .setName("Manoj Babu")
+                .build();
+        System.out.println("Person : " + person);
+    }
+}
